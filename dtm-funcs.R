42d1fe9fa605b5fa2dd4506d564a97933a524227
Ketan Sharma <ketansahils@gmail.com>
2018-05-10 21:15:02 +0530

    add phrase detector

--- dtm-funcs.R
+++ dtm-funcs.R
@@ -22,3 +22,54 @@ remove.whitespace <- function(x)
   x  =  gsub("^\\s+|\\s+$", "", x)
   return(x)
 }
+
+phrase_detector <- function(doc_df, noun=TRUE){ 
+  
+  if (!("pos_tag" %in% colnames(doc_df))) {doc_df = py.annotate(doc_df)}	
+  
+  # defining phrase components
+  verb_phrase = c("RB", "RBR", "RBS", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ", "PRP", "PRP$")
+  noun_phrase = c("JJ", "JJR", "JJS", "NN", "NNP", "NNS", "NNPS")
+  
+  # evaluate condn on phrase type required.  
+  if (noun == "FALSE"){phrase_compts = verb_phrase} else {phrase_compts = noun_phrase}
+  
+  # index the tokens, drop tokens with irrelevant POSTags, etc.  
+  serial_num = seq(1:nrow(doc_df))
+  phrase_index = rep(0, nrow(doc_df))
+  doc_df1 = data.frame(serial_num, phrase_index, doc_df)
+  logical_vec = (doc_df$pos_tag %in% phrase_compts)
+  doc_df_subsetted = doc_df1[logical_vec,]
+  
+  # drop all rows with F-T-F pattern
+  n1 = nrow(doc_df_subsetted)
+  try(if(n1 < 2) stop("not enough df rows"))
+  if (n1 > 5) { 
+    current = doc_df_subsetted$serial_num
+    before = c(0, doc_df_subsetted$serial_num[1:(n1-1)])  
+    after = c(doc_df_subsetted$serial_num[2:n1], 0)
+    drop = ((current - before) != 1)*((after-current) != 1); # sum(drop)
+    
+    doc_df_subsetted = doc_df_subsetted[(!drop),]
+  }
+  
+  # exception handling for when the DF is too small.
+  try(if(nrow(doc_df_subsetted) <3) stop("not enough df rows"))
+  if(nrow(doc_df_subsetted) <3)  {b0 = NULL} else {
+    
+    # build loop for detecting phrases    
+    index = 1
+    for (i1 in 2:nrow(doc_df_subsetted)){
+      if ((doc_df_subsetted$serial_num[i1] - doc_df_subsetted$serial_num[i1-1]) == 1) {
+        doc_df_subsetted$phrase_index[i1] = index;  
+        doc_df_subsetted$phrase_index[i1-1] = index} else {index = index+1}
+    }
+    
+    
+    b0 = sapply(seq(2:max(doc_df_subsetted$phrase_index)),
+                function(x) {
+                  paste0(doc_df_subsetted$token[(doc_df_subsetted$phrase_index == x)], collapse = " ") }) # 0.02 secs
+    
+  }
+  return(b0)
+}
